05a5651e8f4d60fc99d1518c63601f92c706b788
Uriel-ST <urielst45@gmail.com>
2024-12-22 21:13:24 -0600

    Formulario funcional

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const editBtn = document.querySelector("#edit-btn");
+const closeBtn = document.querySelector("#close-btn");
+const saveBtn = document.querySelector("#save-btn");
+const userName = document.querySelector("#user-name");
+const userBiography = document.querySelector("#user-biography");
+const popUp = document.querySelector(".popup");
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name-input");
+const biographyInput = document.querySelector("#biography-input");
+
+editBtn.addEventListener("click", () => {
+  popUp.classList.add("popup_opened");
+
+  // Cargar los valores actuales del perfil en los inputs
+  nameInput.value = userName.textContent.trim();
+  biographyInput.value = userBiography.textContent.trim();
+});
+
+closeBtn.addEventListener("click", () => {
+  popUp.classList.remove("popup_opened");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  popUp.classList.remove("popup_opened");
+});
+
+// Guardar los cambios al enviar el formulario
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault(); // Evitar el comportamiento predeterminado del formulario
+
+  // Actualizar los valores del perfil con los valores de los inputs
+  userName.textContent = nameInput.value.trim();
+  userBiography.textContent = biographyInput.value.trim();
+
+  // Cerrar el popup después de guardar los cambios
+  popUp.classList.remove("popup_opened");
+});
